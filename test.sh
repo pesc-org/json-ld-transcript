747589222188e418226c1baecab7f5c06f24df67
Phil Barker <phil.barker@pjjk.co.uk>
2024-08-21 12:00:29 +0100

    factor Organization validation into own TAP / SHACL, with tests

--- test.sh
+++ test.sh
@@ -1,11 +1,25 @@
 #! /usr/bin/bash
 printf '\nTesting Contact shapes\n'
 printf '======================\n'
-printf '\nTest mininmal passing\n'
+printf '\nTest minimal passing\n'
 pyshacl -s TAP+SHACL/contact-shacl.ttl TestFiles/minimal_passing.ttl
-printf "\nTest maxinmal passing person\n"
+printf "\nTest maximal passing person\n"
 pyshacl -s TAP+SHACL/contact-shacl.ttl TestFiles/max_passing_person.ttl
-printf "\nTest maxinmal passing course\n"
+printf "\nTest maximal passing course\n"
 pyshacl -s TAP+SHACL/contact-shacl.ttl TestFiles/max_passing_course.ttl
 printf "\nBatch Test failing data\n"
 batchshacl TAP+SHACL/contact-shacl.ttl TestFiles/ContactTests
+
+printf '\nTesting Organization shapes\n'
+printf '======================\n'
+printf '\nTest minimal passing\n'
+pyshacl -s TAP+SHACL/organization-shacl.ttl TestFiles/minimal_passing.ttl
+printf '\nTest minimal passing\n'
+pyshacl -s TAP+SHACL/organization-shacl.ttl TestFiles/minimal_passing.ttl
+printf "\nTest maximal passing person\n"
+pyshacl -s TAP+SHACL/organization-shacl.ttl TestFiles/max_passing_person.ttl
+printf "\nTest maximal passing course\n"
+pyshacl -s TAP+SHACL/organization-shacl.ttl TestFiles/max_passing_course.ttl
+printf "\nBatch Test failing data\n"
+batchshacl TAP+SHACL/organization-shacl.ttl TestFiles/OrganizationTests
+printf '\nTesting Organization Data shapes\n'
