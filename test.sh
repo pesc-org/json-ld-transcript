6366861a51ac0f394ae1bf2632f13a8490d7484f
Phil Barker <phil.barker@pjjk.co.uk>
2024-08-21 12:34:48 +0100

    factor Person validation into own TAP & SHACL, with separate tests

--- test.sh
+++ test.sh
@@ -22,4 +22,16 @@ printf "\nTest maximal passing course\n"
 pyshacl -s TAP+SHACL/organization-shacl.ttl TestFiles/max_passing_course.ttl
 printf "\nBatch Test failing data\n"
 batchshacl TAP+SHACL/organization-shacl.ttl TestFiles/OrganizationTests
-printf '\nTesting Organization Data shapes\n'
+
+printf '\nTesting Person shapes\n'
+printf '======================\n'
+printf '\nTest minimal passing\n'
+pyshacl -s TAP+SHACL/person-shacl.ttl TestFiles/minimal_passing.ttl
+printf '\nTest minimal passing\n'
+pyshacl -s TAP+SHACL/person-shacl.ttl TestFiles/minimal_passing.ttl
+printf "\nTest maximal passing person\n"
+pyshacl -s TAP+SHACL/person-shacl.ttl TestFiles/max_passing_person.ttl
+printf "\nTest maximal passing course\n"
+pyshacl -s TAP+SHACL/person-shacl.ttl TestFiles/max_passing_course.ttl
+printf "\nBatch Test failing data\n"
+batchshacl TAP+SHACL/person-shacl.ttl TestFiles/PersonTests
