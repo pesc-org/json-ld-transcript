4d84bef429f33723ed88ca60232aeba692e6fbfa
Phil Barker <phil.barker@pjjk.co.uk>
2024-12-12 17:44:58 +0000

    failing tests for AcademicAward + fixes as required

--- test.sh
+++ test.sh
@@ -39,11 +39,20 @@ pyshacl -s TAP+SHACL/course-related-shacl.ttl TestFiles/max_passing.ttl
 printf "\nBatch Test failing data\n"
 batchshacl TAP+SHACL/course-related-shacl.ttl TestFiles/CourseRelatedTests
 
+printf '\nTesting Achievement Related shapes\n'
+printf '======================\n'
+printf '\nTest minimal passing\n'
+pyshacl -s TAP+SHACL/achieve-related-shacl.ttl TestFiles/minimal_passing.ttl
+printf "\nTest maximal passing\n"
+pyshacl -s TAP+SHACL/achieve-related-shacl.ttl TestFiles/max_passing.ttl
+printf "\nBatch Test failing data\n"
+batchshacl TAP+SHACL/achieve-related-shacl.ttl TestFiles/AchievementRelatedTests
+
+
+
 printf '\nTesting the remaining shapes\n'
 printf '======================\n'
 printf '\nTest minimal passing\n'
 pyshacl -s TAP+SHACL/shacl.ttl TestFiles/minimal_passing.ttl
 printf "\nTest maximal passing \n"
 pyshacl -s TAP+SHACL/shacl.ttl TestFiles/max_passing.ttl
-#printf "\nBatch Test failing data\n"
-#batchshacl TAP+SHACL/shacl.ttl TestFiles
